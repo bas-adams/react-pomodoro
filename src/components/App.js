8919c1f6ba8cbffe6a7cfe647ad51d4c04b7230d
bas-adams <basia.adamska@gmail.com>
2019-09-25 21:46:29 +0200

    Extract TimeboxList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import TimeboxList from './TimeboxList';
+import { EditableTimebox } from './remaining-components';
 
-import { TimeboxList, EditableTimebox } from './remaining-components';
 function App() {
   return (
       <div className='App'>
--- src/components/remaining-components.js
+++ src/components/remaining-components.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Timebox from './Timebox';
 import TimeboxEditor from './TimeboxEditor';
-import TimeboxCreator from './TimeboxCreator';
 import CurrentTimebox from './CurrentTimebox';
 
 
@@ -47,55 +45,6 @@ class EditableTimebox extends React.Component {
 }
 
 
-class TimeboxList extends React.Component {
-    state = {
-        timeboxes: [
-            { id: "a", title: "Uczę się CSS in JS", totalTimeInMinutes: 25 },
-            { id: "b", title: "Uczę się SASS", totalTimeInMinutes: 15 },
-            { id: "c", title: "Uczę się BEM", totalTimeInMinutes: 5 },
-        ]
-    }
 
-    addTimebox = (timebox) => {
-        this.setState(prevState => {
-            const timeboxes = [timebox, ...prevState.timeboxes];
-            return { timeboxes };
-        })
-    }
-    removeTimebox = (indexToRemove) => {
-        this.setState(prevState => {
-            const timeboxes = prevState.timeboxes.filter((timebox, index) => index !== indexToRemove);
-            return { timeboxes };
-        })
-    }
-    updateTimebox = (indexToUpdate, updatedTimebox) => {
-        this.setState(prevState => {
-            const timeboxes = prevState.timeboxes.map((timebox, index) =>
-                index === indexToUpdate ? updatedTimebox : timebox
-            )
-            return { timeboxes };
-        })
-    }
 
-    handleCreate = (createdTimebox) => {
-        this.addTimebox(createdTimebox);
-    }
-    render() {
-        return (
-            <>
-                <TimeboxCreator onCreate={this.handleCreate} />
-                {this.state.timeboxes.map((timebox, index) => (
-                    <Timebox 
-                        key={timebox.id} 
-                        title={timebox.title} 
-                        totalTimeInMinutes={timebox.totalTimeInMinutes}
-                        onDelete={() => this.removeTimebox(index)}
-                        onEdit={() => this.updateTimebox(index, {...timebox, title: "Updated timebox"})}
-                    />
-                ))}
-            </>
-        )
-    }
-}
-
-export { EditableTimebox, TimeboxList }
+export { EditableTimebox }
