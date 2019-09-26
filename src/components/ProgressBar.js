d60dc7f3962ab4180e75b4992afcfcf08a463bd4
bas-adams <basia.adamska@gmail.com>
2019-09-26 10:32:02 +0200

    New modificator for ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProgressBar({ className = "", percent = 33 }) {
     return (
-        <div className={"progress progress--color-red" + className}>
+        <div className={"progress progress--color-green" + className}>
             <div className={"progress__bar"} style={{width: `${percent}%`}}></div>
         </div>
     );
