7edfb7755b3d9ea443776ba0abdc69f23af04d55
bas-adams <basia.adamska@gmail.com>
2019-09-26 10:38:39 +0200

    Add BEM styles for Clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Clock({ className = "", minutes = 20, seconds = 48 }) {
-    return <h2 className={"Clock " + className}>Pozostało {minutes}:{seconds}</h2>
+    return <h2 className={"clock " + className}>Pozostało 
+        <span className="clock--minutes"> {minutes}</span>
+        <span className="clock--separator">:</span>
+        <span className="clock--seconds">{seconds}</span>
+    </h2>
 }
 
 export default Clock;
