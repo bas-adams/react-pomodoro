b24b3706202cff14e19e23514ffefd726524f25a
badamska <basia.adamska@gmail.com>
2020-10-04 13:48:21 +0200

    Extract TimebcoCreator

--- src/components/remaining-componetns.js
+++ src/components/remaining-componetns.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { v4 as uuid } from 'uuid';
 
 import Timebox from './Timebox';
 import TimeboxEditor from './TimeboxEditor';
+import TimeboxCreator from './TimeboxCreator';
 import CurrentTimeBox from './CurrentTimeBox';
 import TimeboxListEditor from './TimeboxListEditor';
 
@@ -54,82 +54,6 @@ class EdiatbelTimebox extends React.Component {
         )
     }
 }
-
-class TimeboxCreator extends React.Component {
-
-    constructor(props){
-        super();
-        //specjalne obiekty które pozowlą nam przechowywać referencje do pól formularza
-        //tworzymy je za pomocą funkcji z API reacta o nazwie createRef
-        //jedna będzie trzymała pole tytułu a druga całkowity czas w minutach
-
-        this.titleInput = React.createRef();
-        this.totalTimeInMinutesInput = React.createRef();
-    }
-
-
-    // state = {
-    //     title: '',
-    //     totalTimeInMinutes: ''
-    // }
-
-    // handleTitleChange = (event) => {
-    //     this.setState({
-    //         title: event.target.value
-    //     });
-    // }
-
-    // handleTotalTimeInMinutesChange = ( event ) => {
-    //     this.setState({
-    //         totalTimeInMinutes: event.target.value
-    //     })
-    // }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.props.onCreate({
-            id: uuid,
-            //title: this.state.title,
-            //totalTimeInMinutes: this.state.totalTimeInMinutes
-            title: this.titleInput.current.value,
-            totalTimeInMinutes: this.totalTimeInMinutesInput.current.value
-        });
-        //czyszczenie formularza jak są dane w state
-        //this.setState({title: '', totalTimeInMinutes: ''});
-        
-        //czyszczenie formularza jak są dane z ref
-        this.titleInput.current.value = '';
-        this.totalTimeInMinutesInput.current.value = '';
-    }
-
-    render() {
-        return(
-            /* w input zamiast value to defaultValue */
-            <form onSubmit = {this.handleSubmit}
-                className = "TimeboxCreator">
-
-                <label>Co robisz?
-                    <input
-                    //usuwamy kod synchronizujący wartosc ze stanem ale dodajemy specjalne property ref do którego przypisujemy naszą zmienną referncyjną.
-                       // value={this.state.title}
-                       // onChange={this.handleTitleChange}
-                       ref = { this.titleInput }
-                        type="text"
-                    />
-                </label><br />
-                <label>Ile minut?
-                    <input
-                        //value={this.state.totaltimeInMinutes}
-                        //onChange={this.handleTotalTimeInMinutesChange}
-                        ref = {this.totalTimeInMinutesInput}
-                        type="numbe"
-                    />
-                </label><br />
-                <button>Dodaj timebox</button>
-            </form>
-        );
-    }
-};
 
 class TimeboxList extends React.Component {
     state = {
